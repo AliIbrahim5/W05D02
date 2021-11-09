10ca8f1f99b782c771ee7e4d4162d8f047de354b
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-09 22:14:50 +0300

    p

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { json } = require("express");
+
 const express =require("express");
 const app =express();
 const fs =require("fs")
@@ -6,29 +6,27 @@ const PORT =5000;
 app.use(express.json());
 
 //read
-app.get("/moves",(seq,res)=>{
+app.get("/moves",(req,res)=>{
     fs.readFile('./move.json',(err,data)=>{
         const moves=JSON.parse(data.toString())
         res.status(200).json(moves)
     })
    
 })
-
-//creat
 function addToFill(moves){
     fs.writeFile('./move.json',JSON.stringify(moves),()=>{
         console.log('Add from move');
     })
 }
-app.post("/addmove",(req,res)=>{
-    const move =req.body
-    fs.readFile('./move.json',(err,data)=>{
-        let newmove =JSON.parse(data.toString());
-        newmove.push({id:newmove.length,name:"jojman",isVefr:false})
-        addToFill(newmove);
-        res.status(200).json(newmove);
-        
 
+//creat
+app.post("/addmove",(req,res)=>{
+    const {name} =req.body
+    fs.readFile('./move.json',(err,data)=>{
+        const newmove =JSON.parse(data.toString());
+        newmove.push({id:newmove.length +1,name,isVefr:false,isdelete:false})
+        addToFill(newmove);
+        res.status(200).json(newmove);    
     })
     
   
@@ -65,44 +63,57 @@ app.put("/delete",(req,res)=>{
 //delete with delete
 
 
-app.delete("/delete1",(req,res)=>{
-    const deleteFrommove =req.body.id
-    fs.readFile('./move.json',(err,data)=>{
-        const moves =JSON.parse(data.toString())
-        const upmoves =moves.map((elem)=>{
-            if(elem.id === deleteFrommove){
-                elem.isdelete =true
-            }
+// app.get("/delete1",(req,res)=>{
+// const uid =req.query.id
+//     console.log(uid);
+    // fs.readFile('./move.json',(err,data)=>{
+        // console.log(data);
+        // const moves =JSON.parse(data.toString())
+        // moves.forEach((ele)=>{
+        //     if(ele.id == id){
+        //         ele.isdelete =true
+        //     }
+        // });
 
-  return elem;
+        
 
-        });
-
-        addToFill(upmoves);
-        res.status(200).json(upmoves)
-    })
+        // addToFill(moves);
+        // res.status(200).json(uid)
+    // })
    
    
-})
-
+// })
+ 
 //updaet
-app.put('/updaet',(req,res)=>{
-    const updetmoves =req.body;
-    fs.readFile('./move.json',(err,data)=>{
-        const moves =JSON.parse(data.toString());
-        const movesToUpdeat= moves.map((elem)=>{
-            if(elem.id ===updetmoves.id){
-                elem.isVefr =updetmoves.isVefr;
-            }
+// app.put('/updaet',(req,res)=>{
+//     const{id,name,isVefr,isdelete} =req.body;
+//     let check =false;
+//     fs.readFile('./move.json',(err,data)=>{
+//         const moves =JSON.parse(data.toString());
+//          moves.forEach((ele)=>{
+//             if(id == updetmoves.id){
+               
+//                 if(name !== undefined)
+//                     ele.name =name;
+                
+//                 if(isVefr !== undefined)
+//                     ele.isVefr =isVefr;
+                
+//                  if( isdelete !== undefined)
+//                     ele.isdelete =isdelete;
 
-            return elem;
+//                 check = true;
+//             } 
+           
 
-        })
+           
+
+//         })
         addToFill(movesToUpdeat);
         res.status(200).json(movesToUpdeat)
     })
 
-    
+   
         
     });
     
